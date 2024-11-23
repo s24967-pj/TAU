5c00324bcc8dfc1f435d8697aefb7ad76809f12c
s24967-pj <s24967@pjwstk.edu.pl>
2024-11-23 20:55:31 +0100

    creating files and classes

---
+++ game.py
@@ -0,0 +1,13 @@
+from board import Board
+from player import Player
+
+
+class Game():
+    def __init__(self, board: Board, player: Player):
+        self.board = board
+        self.player = player
+
+if __name__ == "__main__":
+    board = Board()
+    player = Player()
+    game = Game(board, player)
