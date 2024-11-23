80525be9fd96b2d8e80bbd19146e8e89ddce60a3
s24967-pj <s24967@pjwstk.edu.pl>
2024-11-23 21:58:11 +0100

    Getting move from Player

--- player.py
+++ player.py
@@ -1,4 +1,16 @@
 class Player():
+    MOVES = {
+            "w": (-1, 0),   # Góra
+            "s": (1, 0),    # Dół
+            "a": (0, -1),   # Lewo
+            "d": (0, 1)     # Prawo
+        }   
 
-    def __init__(self):
-        return
+    def get_move(self):
+        move_input = input("Podaj ruch (w=Góra, s=Dół, a=Lewo, d=Prawo): ").strip().lower()
+
+        if move_input in self.MOVES:
+            return self.MOVES[move_input]
+        else:
+            print("Nie możesz się tam ruszyć!")
+            return 0
