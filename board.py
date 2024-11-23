0e245b86268afc6ea7e581bcdf2f107de4dca027
s24967-pj <s24967@pjwstk.edu.pl>
2024-11-23 21:40:21 +0100

    Methods for Board class - Generate, Display, Update

--- board.py
+++ board.py
@@ -1,4 +1,38 @@
+import random
+from colorama import Fore, Style, init
+
 class Board():
 
-    def __init__(self):
-        return
+    def __init__(self, size: int = 5):
+        init(autoreset=True)
+        self.size = size
+        self.board, self.start, self.finish = self._generate()
+        
+    def _generate(self):
+        board = [["X" for _ in range(self.size)] for _ in range(self.size)]
+        start = [self.size - 1, random.randint(0, self.size - 1)]
+        finish = [0, random.randint(0, self.size -1)]
+        print(f"START {start}")
+        print(f"FINISH {finish}")
+
+        board[start[0]][start[1]] = "A"
+        board[finish[0]][finish[1]] = "B"
+
+        return board, start, finish
+    
+    def display(self):
+        for row in self.board:
+            print(" ".join(row))
+
+    def update_board(self, player_position):
+        updated_board = []       
+        for i, row in enumerate(self.board):
+            new_row = []
+            for j, cell in enumerate(row):
+                if [i, j] == player_position:
+                    new_row.append(Fore.GREEN + "P" + Style.RESET_ALL)
+                else:
+                    new_row.append(cell)
+            updated_board.append(new_row)
+        
+        self.board = updated_board
--- game.py
+++ game.py
@@ -11,3 +11,6 @@ if __name__ == "__main__":
     board = Board()
     player = Player()
     game = Game(board, player)
+
+    board.update_board(board.start)
+    board.display()
