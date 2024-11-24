40c1e3fa944c15dbec03d79da5eecc042350a99e
s24967-pj <s24967@pjwstk.edu.pl>
2024-11-24 01:24:41 +0100

    Added tests for moving out of bounds

--- tests/game_tests.py
+++ tests/game_tests.py
@@ -29,3 +29,31 @@ class GameTests(unittest.TestCase):
         """Test sprawdzający, czy gra jest zakończona, gdy gracz osiągnie pole końcowe."""
         self.player.position = self.board.finish
         self.assertTrue(self.game._is_game_finished())
+ 
+    def test_move_out_of_bounds_up(self):
+        """Test, który sprawdza, czy gracz nie może wyjść poza górną granicę planszy."""
+        player_move = (-1, 0)  # Ruch do góry
+        # Ustawiamy gracza na górnej krawędzi planszy
+        self.player.position = (0, self.player.position[1])
+        self.assertFalse(self.game.is_move_correct(player_move), "Gracz nie powinien móc wyjść poza górną granicę!")
+
+    def test_move_out_of_bounds_down(self):
+        """Test, który sprawdza, czy gracz nie może wyjść poza dolną granicę planszy."""
+        player_move = (1, 0)  # Ruch w dół
+        # Ustawiamy gracza na dolnej krawędzi planszy
+        self.player.position = (self.board.size - 1, self.player.position[1])
+        self.assertFalse(self.game.is_move_correct(player_move), "Gracz nie powinien móc wyjść poza dolną granicę!")
+
+    def test_move_out_of_bounds_left(self):
+        """Test, który sprawdza, czy gracz nie może wyjść poza lewą granicę planszy."""
+        player_move = (0, -1)  # Ruch w lewo
+        # Ustawiamy gracza na lewej krawędzi planszy
+        self.player.position = (self.player.position[0], 0)
+        self.assertFalse(self.game.is_move_correct(player_move), "Gracz nie powinien móc wyjść poza lewą granicę!")
+
+    def test_move_out_of_bounds_right(self):
+        """Test, który sprawdza, czy gracz nie może wyjść poza prawą granicę planszy."""
+        player_move = (0, 1)  # Ruch w prawo
+        # Ustawiamy gracza na prawej krawędzi planszy
+        self.player.position = (self.player.position[0], self.board.size - 1)
+        self.assertFalse(self.game.is_move_correct(player_move), "Gracz nie powinien móc wyjść poza prawą granicę!")
